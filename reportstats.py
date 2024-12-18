43460b944dca74ece80221e3c0ce7fca39c02a0d
Jason Martin <jhmartin@toger.us>
2024-12-18 05:02:38 +0000

    Disable reportstats, but update the url in case I reimplement it
    I can get reasonable stats from the CDN / webserver access logs, don't need a bespoke report mechanism

--- reportstats.py
+++ reportstats.py
@@ -46,7 +46,8 @@ class ReportStats(threading.Thread):
         self._gui_alpha = config.OPTIONS_OBJECT.Get("GuiAlpha", 250)
 
     def run(self):
-        url = "http://pyspy.pythonanywhere.com/add_record/"
+        return
+        url = "http://pyspy.toger.us/add_record/"
         headers = {
             "Accept-Encoding": "gzip",
             "User-Agent": "PySpy, Author: White Russsian, https://github.com/WhiteRusssian/PySpy"
